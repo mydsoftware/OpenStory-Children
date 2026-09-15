861f9cd8969bbb11c0199ae06dd2c1b016c40073
mydsoftware <46159939+mydsoftware@users.noreply.github.com>
2026-09-15 22:24:13 +0330

    feat(core): expose complete platform module

---
+++ packages/core/src/platform.ts
@@ -0,0 +1,6 @@
+export * from "./story-engine.js";
+export * from "./qa.js";
+export * from "./providers.js";
+export * from "./renderer.js";
+export * from "./book-store.js";
+export * from "./orchestrator.js";
